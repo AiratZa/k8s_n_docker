cf3123706a7c15a2ca4bdb59407eceba667f1c42
AiratZa <52579913+AiratZa@users.noreply.github.com>
2020-10-13 14:34:37 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,28 +12,28 @@ eval $(minikube docker-env)
 
 minikube addons enable metallb
 
-kubectl apply -f srcs/yaml_configs_for_k8s/metallb.yaml
+kubectl apply -f srcs/yaml_configs_for_k8s/metallb.yaml || error_ocurred
 
-kubectl apply -f srcs/yaml_configs_for_k8s/volumes.yaml
+kubectl apply -f srcs/yaml_configs_for_k8s/volumes.yaml || error_ocurred
 
-# docker build srcs/nginx -t "nginx_img"
+# docker build srcs/nginx -t "nginx_img" || error_ocurred
 
-# docker build srcs/ftps -t "ftps_img"
+# docker build srcs/ftps -t "ftps_img" || error_ocurred
 
-docker build srcs/mysql -t "mysql_img"
+docker build srcs/mysql -t "mysql_img" || error_ocurred
 
-docker build srcs/wordpress -t "wp_img"
+docker build srcs/wordpress -t "wp_img" || error_ocurred
 
-docker build srcs/phpmyadmin -t "phpmyadm_img"
+docker build srcs/phpmyadmin -t "phpmyadm_img" || error_ocurred
 
 
-# kubectl apply -f srcs/yaml_configs_for_k8s/nginx.yaml
+# kubectl apply -f srcs/yaml_configs_for_k8s/nginx.yaml || error_ocurred
 
-# kubectl apply -f srcs/yaml_configs_for_k8s/ftps.yaml
+# kubectl apply -f srcs/yaml_configs_for_k8s/ftps.yaml || error_ocurred
 
-kubectl apply -f srcs/yaml_configs_for_k8s/mysql.yaml
+kubectl apply -f srcs/yaml_configs_for_k8s/mysql.yaml || error_ocurred
 
-kubectl apply -f srcs/yaml_configs_for_k8s/wordpress.yaml
+kubectl apply -f srcs/yaml_configs_for_k8s/wordpress.yaml || error_ocurred
 
-kubectl apply -f srcs/yaml_configs_for_k8s/phpmyadmin.yaml
+kubectl apply -f srcs/yaml_configs_for_k8s/phpmyadmin.yaml || error_ocurred
 
