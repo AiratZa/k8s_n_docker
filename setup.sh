d99946d715f1fb4634419837e31005441303951d
Great Drake <gdrake@un-o5.kzn.21-school.ru>
2020-10-15 04:19:26 +0300

    Finish

--- setup.sh
+++ setup.sh
@@ -32,19 +32,20 @@ docker build srcs/influxdb -t "influxdb_img"   || error_ocurred
 docker build srcs/grafana -t "grafana_img"     || error_ocurred
 
 
-kubectl apply -f srcs/yaml_configs_for_k8s/nginx.yaml || error_ocurred
+kubectl apply -f srcs/yaml_configs_for_k8s/nginx.yaml      || error_ocurred
 
-kubectl apply -f srcs/yaml_configs_for_k8s/ftps.yaml || error_ocurred
+kubectl apply -f srcs/yaml_configs_for_k8s/ftps.yaml       || error_ocurred
 
-kubectl apply -f srcs/yaml_configs_for_k8s/mysql.yaml || error_ocurred
+kubectl apply -f srcs/yaml_configs_for_k8s/mysql.yaml      || error_ocurred
 
-kubectl apply -f srcs/yaml_configs_for_k8s/wordpress.yaml || error_ocurred
+kubectl apply -f srcs/yaml_configs_for_k8s/wordpress.yaml  || error_ocurred
 
 kubectl apply -f srcs/yaml_configs_for_k8s/phpmyadmin.yaml || error_ocurred
 
-kubectl apply -f srcs/yaml_configs_for_k8s/influxdb.yaml || error_ocurred
+kubectl apply -f srcs/yaml_configs_for_k8s/influxdb.yaml   || error_ocurred
 
-kubectl apply -f srcs/yaml_configs_for_k8s/grafana.yaml || error_ocurred
+kubectl apply -f srcs/yaml_configs_for_k8s/grafana.yaml    || error_ocurred
+
 
 echo "ˆ_ˆ !!! All containers are built and run at the moment !!! ˆ_ˆ"
 
