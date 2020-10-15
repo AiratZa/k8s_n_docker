a11294163cd737583c7c7f349b6cc5293cc723f2
Great Drake <gdrake@un-o5.kzn.21-school.ru>
2020-10-15 03:05:17 +0300

    On road to finish

--- setup.sh
+++ setup.sh
@@ -16,24 +16,35 @@ kubectl apply -f srcs/yaml_configs_for_k8s/metallb.yaml || error_ocurred
 
 kubectl apply -f srcs/yaml_configs_for_k8s/volumes.yaml || error_ocurred
 
-# docker build srcs/nginx -t "nginx_img" || error_ocurred
 
-# docker build srcs/ftps -t "ftps_img" || error_ocurred
+docker build srcs/nginx -t "nginx_img"         || error_ocurred
 
-docker build srcs/mysql -t "mysql_img" || error_ocurred
+docker build srcs/ftps -t "ftps_img"           || error_ocurred
 
-docker build srcs/wordpress -t "wp_img" || error_ocurred
+docker build srcs/mysql -t "mysql_img"         || error_ocurred
+
+docker build srcs/wordpress -t "wp_img"        || error_ocurred
 
 docker build srcs/phpmyadmin -t "phpmyadm_img" || error_ocurred
 
+docker build srcs/influxdb -t "influxdb_img"   || error_ocurred
 
-# kubectl apply -f srcs/yaml_configs_for_k8s/nginx.yaml || error_ocurred
+docker build srcs/grafana -t "grafana_img"     || error_ocurred
 
-# kubectl apply -f srcs/yaml_configs_for_k8s/ftps.yaml || error_ocurred
+
+kubectl apply -f srcs/yaml_configs_for_k8s/nginx.yaml || error_ocurred
+
+kubectl apply -f srcs/yaml_configs_for_k8s/ftps.yaml || error_ocurred
 
 kubectl apply -f srcs/yaml_configs_for_k8s/mysql.yaml || error_ocurred
 
 kubectl apply -f srcs/yaml_configs_for_k8s/wordpress.yaml || error_ocurred
 
 kubectl apply -f srcs/yaml_configs_for_k8s/phpmyadmin.yaml || error_ocurred
+
+kubectl apply -f srcs/yaml_configs_for_k8s/influxdb.yaml || error_ocurred
+
+kubectl apply -f srcs/yaml_configs_for_k8s/grafana.yaml || error_ocurred
+
+echo "ˆ_ˆ !!! All containers are built and run at the moment !!! ˆ_ˆ"
 
